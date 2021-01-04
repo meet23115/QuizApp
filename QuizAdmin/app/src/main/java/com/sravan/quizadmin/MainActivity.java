b9a49c5c90c4edaf9d81b28bb052fdbc9d7882de
SravanAdula <64852183+SravanAdula@users.noreply.github.com>
2021-01-03 23:55:41 -0500

    Invalid Credentials error

--- QuizAdmin/app/src/main/java/com/sravan/quizadmin/MainActivity.java
+++ QuizAdmin/app/src/main/java/com/sravan/quizadmin/MainActivity.java
@@ -3,12 +3,47 @@ package com.sravan.quizadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email, pass;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        login = findViewById(R.id.loginB);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(email.getText().toString().isEmpty()) {
+                    email.setError("Erter Email ID");
+                    return;
+                }
+                else
+                {
+                    email.setError(null);
+                }
+
+                if(pass.getText().toString().isEmpty()) {
+                    pass.setError("Enter Password");
+                    return;
+                }
+                else
+                {
+                    pass.setError(null);
+                }
+            }
+        });
     }
 }
