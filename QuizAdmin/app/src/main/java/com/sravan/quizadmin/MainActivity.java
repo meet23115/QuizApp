452c77c5eb98853a06ac2ed1f83f0614f5d22dc5
SravanAdula <64852183+SravanAdula@users.noreply.github.com>
2021-01-04 16:19:25 -0500

    Firebase Auth

--- QuizAdmin/app/src/main/java/com/sravan/quizadmin/MainActivity.java
+++ QuizAdmin/app/src/main/java/com/sravan/quizadmin/MainActivity.java
@@ -1,16 +1,28 @@
 package com.sravan.quizadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText email, pass;
     private Button login;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         login = findViewById(R.id.loginB);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+
+
+
+
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +73,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void firebaseLogin()
     {
-        
+        firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
+
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
